f33609444bae65edc85705d7da6562596f49ed76
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2025-09-24 10:15:43 +0530

    Integrated Database successfully

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './Config/db.js';
 
 dotenv.config();
 const app = express();
@@ -10,3 +11,5 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+connectDB();
