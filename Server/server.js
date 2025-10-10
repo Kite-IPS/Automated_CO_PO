1b838ee81d80cf2afff64fdd9ef05d1f74de1984
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2025-10-10 10:35:19 +0530

    Completed the copo api

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './Config/db.js';
 import AuthRoute from './Routes/AuthRoute.js';
+import CopoRouter from './Routes/CopoRoute.js';
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : 6000;
 app.use(express.json());
 
 app.use('/auth', AuthRoute)
+app.use('/copo', CopoRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
